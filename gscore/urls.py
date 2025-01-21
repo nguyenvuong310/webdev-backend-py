a26959b8e486b61764f9b365f3861f98648bb427
nguyenvuong310 <trungvuong2169@gmail.com>
2025-01-21 22:48:00 +0700

    fix same origin

--- gscore/urls.py
+++ gscore/urls.py
@@ -4,6 +4,9 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -15,6 +18,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
+    url = os.getenv('API_URL')
 )
 
 urlpatterns = [
